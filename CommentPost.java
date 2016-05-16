a2e24eb6f5805a6338bfa31ff9c9a89da0d04759
paco <franmenendezpouso@gmail.com>
2016-05-16 12:06:55 +0200

    modificadas herencias y creada clase CommentPost

---
+++ CommentPost.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+/**
+ * Write a description of class CommentPost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CommentPost extends Post
+{
+    // instance variables - replace the example below with your own
+     private ArrayList<String>comments;
+
+    /**
+     * Constructor for objects of class CommentPost
+     */
+    public CommentPost()
+    {
+      
+       super("xxx");   
+       comments = new ArrayList<>();
+    
+    }
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void addComents()
+    {
+        String info = "";
+        if(comments.size() != 0)
+            for(int i=0; i<comments.size(); i++)
+                info += comments.get(i) + "\n_____________________\n";
+        else
+            info += "Sin comentarios";
+    }
+}
